e4427f819b91453fb03c01afe28c38e3c428c279
sunye <sunie@buaa.edu.cn>
2024-05-15 17:52:19 +0800

    修复文件路径

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-# pkill -f "inference.py"
-ps -ef | grep "inference.py" | grep -v grep | awk '{print $2}' | xargs kill -9
+pkill -f "app.py"
+# ps -ef | grep "app.py" | awk '{print $2}' | xargs kill -9
 
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate torch
 
-nohup python inference.py 2>&1 > inference.log & 
+nohup python app.py 2>&1 > app.log & 
 
 
 # conda activate torch
